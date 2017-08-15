037d3cc7fc6e46d377823260261ec879692d10ac
Bob Carroll <Bob Carroll>
2017-08-15 13:54:43 -0400

    copy and first commit of burger repo

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Godfather');
+INSERT INTO burgers (burger_name) VALUES ('Meatball');
+INSERT INTO burgers (burger_name) VALUES ('Nothing');
+INSERT INTO burgers (burger_name) VALUES ('Mooch');
+INSERT INTO burgers (burger_name) VALUES ('Boom');
+INSERT INTO burgers (burger_name) VALUES ('Sparkle');
+
